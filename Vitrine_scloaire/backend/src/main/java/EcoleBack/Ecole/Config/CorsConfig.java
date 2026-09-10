058a8cf9cfc0cda534343038cdb5ae89568fb1a3
Djire <odjidev@gmail.com>
2026-09-10 14:12:42 +0000

    Ajout du site complet et avec spring boot gestion des actualite

---
+++ Vitrine_scloaire/backend/src/main/java/EcoleBack/Ecole/Config/CorsConfig.java
@@ -0,0 +1,27 @@
+
+package EcoleBack.Ecole.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        
+        config.setAllowCredentials(false); // Mettre à true si vous envoyez des cookies ou des en-têtes Authorization
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
